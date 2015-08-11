7875099929657d2fc4eeb4aac221528d89ed288c
Marcus Fulbright <marcusf@patientfocus.com>
2015-08-11 17:13:31 -0500

    add location drop down

--- views/employee_form.php
+++ views/employee_form.php
@@ -14,6 +14,17 @@ $edit = isset($this->data) ? true : false;
     Last Name: <input type="text" name="lastName" value="<?php if($edit) {echo $this->data['lastName'];} ?>"> <br>
     Phone Number: <input type="text" name="phone" placeholder="555-555-5555" value="<?php if($edit) { echo $this->data['phone'];}?>">
     email: <input type="text" name="email" value="<?php if($edit){ echo $this->data['email'];} ?>">
+    location:
+    <select>
+        <?php foreach ($this->locations as $location) {
+            if ($edit && $this->data['location'] === $location->getId()) {
+                echo '<option selected="selected" value="'.$location->getId().'">'.$location->getName().'</option>';
+            } else {
+                echo '<option value="'.$location->getId().'">'.$location->getName().'</option>';
+            }
+        }
+        ?>
+    </select>
     <input type="submit" value="Submit">
 </form>
 </body>
