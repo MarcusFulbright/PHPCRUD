cbf021c631cefad9eec43eb6d9fdaa8cbc629ff1
Marcus Fulbright <marcusf@patientfocus.com>
2015-08-12 10:39:44 -0500

    add properties and getters / setters

--- src/Entities/Employee.php
+++ src/Entities/Employee.php
@@ -15,6 +15,18 @@ class Employee extends Entity
 
     protected static $table ='employees';
 
+    protected $id;
+
+    protected $firstName;
+
+    protected $lastName;
+
+    protected $phone;
+
+    protected $email;
+
+    protected $location;
+
     public static function fields()
     {
         return [
@@ -32,5 +44,65 @@ class Employee extends Entity
         return [
             $mapper->belongsTo($entity, 'Mbright\Entities\Location', 'location')
         ];
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getFirstName()
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName($firstName)
+    {
+        $this->firstName = $firstName;
+    }
+
+    public function getLastName()
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName($lastName)
+    {
+        $this->lastName = $lastName;
+    }
+
+    public function getPhone()
+    {
+        return $this->phone;
+    }
+
+    public function setPhone($phone)
+    {
+        $this->phone = $phone;
+    }
+
+    public function getEmail()
+    {
+        return $this->email;
+    }
+
+    public function setEmail($email)
+    {
+        $this->email = $email;
+    }
+
+    public function getLocation()
+    {
+        return $this->location;
+    }
+
+    public function setLocation($location)
+    {
+        $this->location = $location;
     }
 }
--- src/Entities/Location.php
+++ src/Entities/Location.php
@@ -7,11 +7,35 @@ class Location extends Entity
 {
     protected static $table ='locations';
 
+    protected $id;
+
+    protected $name;
+
     public static function fields()
     {
         return [
             'id'    => ['type' => 'integer', 'primary' => true, 'autoincrement' => true],
             'name'  => ['type' => 'string', 'length' => 50]
         ];
+    }
+
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function setName($name)
+    {
+        $this->name = $name;
     }
 }
